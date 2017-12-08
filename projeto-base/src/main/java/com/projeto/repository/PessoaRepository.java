8f03b3c80b0480161e21573a9141730ce8291c91
bilhares <felipe.bilhares@hotmail.com>
2017-12-08 17:08:08 -0300

    Projeto base com autenticação permissao de usuario oauth2 rest

---
+++ projeto-base/src/main/java/com/projeto/repository/PessoaRepository.java
@@ -0,0 +1,14 @@
+package com.projeto.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import com.projeto.model.Pessoa;
+
+public interface PessoaRepository extends JpaRepository<Pessoa, Long> {
+
+	@Query(value = "SELECT * FROM PESSOA WHERE ESTADO LIKE %?1%", nativeQuery = true)
+	public List<Pessoa> findByEndereco(String endereco);
+}
---
+++ projeto-base/src/main/java/com/projeto/resource/PessoaResource.java
@@ -0,0 +1,53 @@
+package com.projeto.resource;
+
+import java.net.URI;
+import java.util.List;
+
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.projeto.model.Pessoa;
+import com.projeto.repository.PessoaRepository;
+
+@RestController
+@RequestMapping("/pessoas")
+public class PessoaResource {
+	@Autowired
+	private PessoaRepository pessoaRepository;
+
+	@PostMapping
+	@PreAuthorize("hasAuthority('ROLE_CADASTRAR_PESSOA') and #oauth2.hasScope('write')")
+	public ResponseEntity<Pessoa> criar(@Valid @RequestBody Pessoa pessoa, HttpServletResponse response) {
+		Pessoa pessoaSalva = pessoaRepository.save(pessoa);
+
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{codigo}")
+				.buildAndExpand(pessoaSalva.getCodigo()).toUri();
+		response.setHeader("Location", uri.toASCIIString());
+
+		return ResponseEntity.created(uri).body(pessoaSalva);
+	}
+
+	@GetMapping("/{codigo}")
+	@PreAuthorize("hasAuthority('ROLE_PESQUISAR_PESSOA') and #oauth2.hasScope('read')")
+	public ResponseEntity<Pessoa> buscarPeloCodigo(@PathVariable Long codigo) {
+		Pessoa pessoa = pessoaRepository.findOne(codigo);
+		return pessoa != null ? ResponseEntity.ok(pessoa) : ResponseEntity.notFound().build();
+	}
+	
+	@RequestMapping(value = "/find/{filtro}", method = RequestMethod.GET)
+	List<Pessoa> findByFiltro(@PathVariable("filtro") String endereco) {
+		return pessoaRepository.findByEndereco(endereco);
+	}
+}
